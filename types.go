ce4750e793460e4bfe0882dcfb4bb6975299ec10
Dagime Teshome <dagimeteshome1@gmail.com>
2026-09-21 17:16:43 +0300

    add user response data types

--- types.go
+++ types.go
@@ -1,7 +1,12 @@
 package main
 
-type Req_body struct {
-	Body string `json:"body"`
+import (
+	"time"
+)
+
+type chirp_body struct {
+	Body    string `json:"body"`
+	User_id string `json:"user_id"`
 }
 
 type Res_body struct {
@@ -14,4 +19,14 @@ type clean_body struct {
 
 type err_resp struct {
 	Error string `json:"error"`
+}
+
+type user_create struct {
+	Email string `json:"email"`
+}
+type user_response struct {
+	ID         string    `json:"id"`
+	Created_at time.Time `json:"created_at"`
+	Updated_at time.Time `json:"updated_at"`
+	Email      string    `json:"email"`
 }
